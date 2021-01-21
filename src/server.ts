066a5fb9729aa85366848dddda0803284de085f2
Fei <33122873+pheihuihui@users.noreply.github.com>
2021-01-21 23:33:08 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import * as http from 'http'
+import { initGlobalStatus } from './utilities/server/global';
 import { cron_every_hour } from './utilities/server/schedules';
 
-// Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-
-    // Send back a response and end the connection
     res.end('Hello World!\n')
 });
 
+initGlobalStatus()
 cron_every_hour.start()
-// Start the server on port 3000
 const port = process.env.PORT || 3000
 app.listen(port)
